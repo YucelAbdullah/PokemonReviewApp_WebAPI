dcedb62e60fb0bc5dfd5fd606671906b4ace8a4b
Abdullah YÜCEL <abdullahyuucel@gmail.com>
2022-12-15 00:28:21 +0300

    repository pattern

---
+++ PokemonReviewApp/Interfaces/IPokemonRepository.cs
@@ -0,0 +1,17 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface IPokemonRepository
+    {
+        ICollection<Pokemon> GetPokemons();
+
+        Pokemon GetPokemon(int id);
+        Pokemon GetPokemon(string name);
+
+        decimal GetPokemonRatig(int pokeId);
+        bool PokemonExisting(int pokeId);
+
+
+    }
+}
